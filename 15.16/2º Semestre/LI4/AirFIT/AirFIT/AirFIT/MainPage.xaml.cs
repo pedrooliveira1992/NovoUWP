d4168753ce37c586d338e5386c3dcba291199a8f
Pedro Oliveira <poliveira199208@gmail.com>
2016-05-12 17:56:24 +0100

    O que ja fiz

---
+++ 15.16/2º Semestre/LI4/AirFIT/AirFIT/AirFIT/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AirFIT
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            BackButton.Visibility = Visibility.Collapsed;
+            MyFrame.Navigate(typeof(Home));
+            TitleTextBlock.Text = "Home";
+            HomeListBoxItem.IsSelected = true;
+        }
+
+        private void backOfficeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyFrame.CanGoBack)
+            {
+                MyFrame.GoBack();
+                HomeListBoxItem.IsSelected = true;
+            }
+        }
+
+
+        private void IconsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (HomeListBoxItem.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Collapsed;
+                MyFrame.Navigate(typeof(Home));
+                TitleTextBlock.Text = "Home";
+            }
+            else if (SessaoTrabalhoListBoxItem.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Visible;
+                MyFrame.Navigate(typeof(SessaoTrabalho));
+                TitleTextBlock.Text = "Sessão de Trabalho";
+            }
+            else if (AtividadesListBoxItem.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Visible;
+                MyFrame.Navigate(typeof(Atividades));
+                TitleTextBlock.Text = "Atividades";
+            }
+            else if (MailListBoxItem.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Visible;
+                MyFrame.Navigate(typeof(Mail));
+                TitleTextBlock.Text = "Mail";
+            }
+        }
+    }
+}
